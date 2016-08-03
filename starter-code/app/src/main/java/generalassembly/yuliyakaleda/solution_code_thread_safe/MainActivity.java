d3af21ff0561cd379a10ecae4b042e107e22b3da
eric.kivet <eric.kivet@gmail.com>
2016-08-03 10:08:50 -0400

    threading-lab

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity{
     if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
       Uri selectedImage = data.getData();
       //TODO: Create the async task and execute it
+      new ImageProcessingAsyncTask().execute(selectedImage);
     }
   }
 
@@ -62,11 +63,11 @@ public class MainActivity extends AppCompatActivity{
   }
 
   //TODO: Fill in the parameter types
-  private class ImageProcessingAsyncTask extends AsyncTask<> {
+  private class ImageProcessingAsyncTask extends AsyncTask<Uri, Integer, Bitmap> {
 
     //TODO: Fill in the parameter type
     @Override
-    protected Bitmap doInBackground() {
+    protected Bitmap doInBackground(Uri... params) {
       try {
         Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(params[0]));
         return invertImageColors(bitmap);
@@ -78,21 +79,25 @@ public class MainActivity extends AppCompatActivity{
 
     //TODO: Fill in the parameter type
     @Override
-    protected void onProgressUpdate() {
+    protected void onProgressUpdate(Integer... values) {
       super.onProgressUpdate(values);
       //TODO: Update the progress bar
+      mProgressBar.setProgress(values[0]);
     }
 
     //TODO: Fill in the parameter type
     @Override
-    protected void onPostExecute() {
+    protected void onPostExecute(Bitmap bitmap) {
       //TODO: Complete this method
+      mProgressBar.setVisibility(View.INVISIBLE);
+      mImageView.setImageBitmap(bitmap);
     }
 
     @Override
     protected void onPreExecute() {
       super.onPreExecute();
       //TODO: Complete this method
+      mProgressBar.setVisibility(View.VISIBLE);
     }
 
     private Bitmap invertImageColors(Bitmap bitmap){
@@ -103,10 +108,23 @@ public class MainActivity extends AppCompatActivity{
       for (int i = 0; i < mutableBitmap.getWidth(); i++) {
         for(int j = 0; j < mutableBitmap.getHeight(); j++){
           //TODO: Get the Red, Green, and Blue values for the current pixel, and reverse them
+          int pixel = mutableBitmap.getPixel(i,j);
+          int pixelBlue = Color.blue(pixel);
+          int pixelRed = Color.red(pixel);
+          int pixelGreen = Color.green(pixel);
+
           //TODO: Set the current pixel's color to the new, reversed value
+          int oppBlue = 255 - pixelBlue;
+          int oppRed = 255 - pixelRed;
+          int oppGreen = 255 - pixelGreen;
+
+          int invertedColor = Color.rgb(oppBlue,oppRed,oppGreen);
+
+          mutableBitmap.setPixel(i,j,invertedColor);
         }
         int progressVal = Math.round((long) (100*(i/(1.0*mutableBitmap.getWidth()))));
         //TODO: Update the progress bar. progressVal is the current progress value out of 100
+        publishProgress(progressVal);
       }
       return mutableBitmap;
     }
